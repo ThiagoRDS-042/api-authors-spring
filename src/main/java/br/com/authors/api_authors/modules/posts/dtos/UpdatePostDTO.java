e51073add575aad22958d73f89bbdaee948d1d23
ThiagoRDS <thiagords042@gmail.com>
2025-02-28 11:12:48 -0300

    feat: add update posts

---
+++ src/main/java/br/com/authors/api_authors/modules/posts/dtos/UpdatePostDTO.java
@@ -0,0 +1,16 @@
+package br.com.authors.api_authors.modules.posts.dtos;
+
+import java.util.UUID;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+public record UpdatePostDTO(
+                @NotBlank UUID postId,
+                @NotBlank String title,
+                @NotBlank String content,
+                @NotBlank String description,
+                @NotNull @Size(min = 1, message = "keywords must be min 1 length") String[] keywords,
+                @NotBlank UUID authorId) {
+}
