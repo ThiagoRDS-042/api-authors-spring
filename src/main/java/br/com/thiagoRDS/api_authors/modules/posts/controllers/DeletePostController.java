4132debd9e78bf704ad9ddcaa96edc5fbc5a271e
ThiagoRDS <thiagords042@gmail.com>
2025-03-07 12:24:20 -0300

    refactor: rename files

--- src/main/java/br/com/thiagoRDS/api_authors/modules/posts/controllers/DeletePostController.java
+++ src/main/java/br/com/thiagoRDS/api_authors/modules/posts/controllers/DeletePostController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.thiagoRDS.api_authors.modules.posts.dtos.DeletePostByIdDTO;
-import br.com.thiagoRDS.api_authors.modules.posts.usecases.DeletePostById;
+import br.com.thiagoRDS.api_authors.modules.posts.usecases.DeletePost;
 import br.com.thiagoRDS.api_authors.utils.SessionId;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
@@ -18,9 +18,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/posts")
-public class DeletePostByIdController {
+public class DeletePostController {
   @Autowired
-  private DeletePostById deletePostById;
+  private DeletePost deletePostById;
 
   @DeleteMapping("/{postId}")
   public ResponseEntity<Object> delete(HttpServletRequest request, @Valid @PathVariable UUID postId) {
