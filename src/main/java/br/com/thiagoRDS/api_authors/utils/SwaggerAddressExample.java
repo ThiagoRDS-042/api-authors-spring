d3ff0b909c0c2c33bae46c6ff0bc63810871ea96
ThiagoRDS <thiagords042@gmail.com>
2025-03-16 20:42:17 -0300

    feat: add swagger utils

---
+++ src/main/java/br/com/thiagoRDS/api_authors/utils/SwaggerAddressExample.java
@@ -0,0 +1,10 @@
+package br.com.thiagoRDS.api_authors.utils;
+
+public record SwaggerAddressExample() {
+  public static final String CITY = "city";
+  public static final String STREET = "street";
+  public static final String ZIP_CODE = "12345-678";
+  public static final String STATE_CODE = "ST";
+  public static final String COMPLEMENT = "complement";
+  public static final String NEIGHBORHOOD = "neighborhood";
+}
