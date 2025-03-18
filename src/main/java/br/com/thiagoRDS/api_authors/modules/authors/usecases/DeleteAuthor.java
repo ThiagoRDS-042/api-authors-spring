041040c8a2d090d375651a50114e9dfa86bc9a63
ThiagoRDS <thiagords042@gmail.com>
2025-03-18 20:09:18 -0300

    feat: add delete file

--- src/main/java/br/com/thiagoRDS/api_authors/modules/authors/usecases/DeleteAuthor.java
+++ src/main/java/br/com/thiagoRDS/api_authors/modules/authors/usecases/DeleteAuthor.java
@@ -4,15 +4,21 @@ import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
+import br.com.thiagoRDS.api_authors.config.MinioConfig;
 import br.com.thiagoRDS.api_authors.modules.authors.entities.Author;
 import br.com.thiagoRDS.api_authors.modules.authors.exceptions.AuthorNotFoundException;
 import br.com.thiagoRDS.api_authors.modules.authors.repositories.AuthorsRepository;
+import br.com.thiagoRDS.api_authors.providers.MinioProvider;
 
 @Service
 public class DeleteAuthor {
+  private final MinioConfig minioConfig;
+  private final MinioProvider minioProvider;
   private final AuthorsRepository authorsRepository;
 
-  public DeleteAuthor(AuthorsRepository authorsRepository) {
+  public DeleteAuthor(MinioConfig minioConfig, MinioProvider minioProvider, AuthorsRepository authorsRepository) {
+    this.minioConfig = minioConfig;
+    this.minioProvider = minioProvider;
     this.authorsRepository = authorsRepository;
   }
 
@@ -20,6 +26,10 @@ public class DeleteAuthor {
     Author author = this.authorsRepository.findById(authorId).orElseThrow(() -> {
       throw new AuthorNotFoundException();
     });
+
+    if (author.getAvatar() != null) {
+      this.minioProvider.deleteFile(this.minioConfig.getBucketName(), author.getAvatar());
+    }
 
     this.authorsRepository.delete(author);
   }
