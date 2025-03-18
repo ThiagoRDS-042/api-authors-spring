bd2154fa2763ab2c938e6434c264c56ea6914cea
ThiagoRDS <thiagords042@gmail.com>
2025-03-18 19:44:40 -0300

    refactor: adjust file name

--- src/main/java/br/com/thiagoRDS/api_authors/modules/authors/controllers/GetAvatarController.java
+++ src/main/java/br/com/thiagoRDS/api_authors/modules/authors/controllers/GetAvatarController.java
@@ -27,27 +27,27 @@ import org.springframework.web.bind.annotation.PathVariable;
 @RequestMapping("/authors")
 public class GetAvatarController {
 
-  @Autowired
-  private GetAvatar getAvatar;
+    @Autowired
+    private GetAvatar getAvatar;
 
-  @Operation(summary = "Get a author avatar", description = "Get a author avatar")
-  @ApiResponses({
-      @ApiResponse(responseCode = "200", content = {
-          @Content(mediaType = MediaType.APPLICATION_OCTET_STREAM_VALUE, schema = @Schema(implementation = InputStreamResource.class))
-      }, description = "Success"),
-      @ApiResponse(responseCode = "404", content = {
-          @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ErrorResponseDTO.class))
-      }, description = "Author does not exists."),
-  })
-  @GetMapping("/avatar/{avatar:.+}")
-  public ResponseEntity<Object> getAvatar(@PathVariable String avatar) {
-    InputStream file = this.getAvatar.execute(avatar);
+    @Operation(summary = "Get a author avatar", description = "Get a author avatar")
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = {
+                    @Content(mediaType = MediaType.APPLICATION_OCTET_STREAM_VALUE, schema = @Schema(implementation = InputStreamResource.class))
+            }, description = "Success"),
+            @ApiResponse(responseCode = "404", content = {
+                    @Content(mediaType = MediaType.APPLICATION_JSON_VALUE, schema = @Schema(implementation = ErrorResponseDTO.class))
+            }, description = "Author does not exists."),
+    })
+    @GetMapping("/avatar/{avatar:.+}")
+    public ResponseEntity<Object> getAvatar(@PathVariable String avatar) {
+        InputStream file = this.getAvatar.execute(avatar);
 
-    InputStreamResource inputStreamResource = new InputStreamResource(file);
+        InputStreamResource inputStreamResource = new InputStreamResource(file);
 
-    return ResponseEntity.ok()
-        .contentType(MediaType.APPLICATION_OCTET_STREAM)
-        .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename='" + avatar + "'")
-        .body(inputStreamResource);
-  }
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_OCTET_STREAM)
+                .header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + avatar)
+                .body(inputStreamResource);
+    }
 }
